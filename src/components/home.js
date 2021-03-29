39c4b7ddb41b172fd0db915e68b512b8f28b4880
Skye Finley <80650005+Skye-Finley@users.noreply.github.com>
2021-03-29 14:38:51 -0500

    Add files via upload

---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+export class Home extends Component{
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+            <h3>Welcome Employee Management Portal.
+                This is the home page.</h3>
+            </div>
+        )
+    }
+}
+
+var numbers = [32, 33, 17, 41];
+
+function checkNumber(Odd) {
+  return number >= Odd;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = number.filter(checkOdd);
+}
